3102dc89007949b07f122dc4fe829b7cffde69c3
Mert Hacioglu <mhacioglu96@gmail.com>
2024-12-07 23:58:02 +0000

    initial commit

---
+++ src/test/java/org/mhacioglu/peaktrackserver/PeaktrackServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mhacioglu.peaktrackserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeaktrackServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
